9671a47705d34e5d3610bc21af28faa23decc608
Unknown <keybladeschoosen1@gmail.com>
2019-07-01 14:03:23 -0400

    Added Orders Get Swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,17 +14,8 @@ const routes = [
           description: "Successful response",
           type: "array",
           items: {
-            $id: "#/items",
             type: "object",
             title: "User Object",
-            required: [
-              "id",
-              "username",
-              "email",
-              "password",
-              "orders",
-              "contacts"
-            ],
             properties: {
               id: {
                 type: "string",
@@ -296,7 +287,104 @@ const routes = [
   {
     method: "GET",
     url: "/api/orders/",
-    handler: orderControllers.getOrders
+    handler: orderControllers.getOrders,
+    schema: {
+      description: "Get a list of all Orders",
+      summary: "Get all orders",
+      tags: ["order"],
+      response: {
+        200: {
+          description: "Successful response",
+          type: "array",
+          items: {
+            type: "object",
+            title: "Order Object",
+            properties: {
+              id: {
+                type: "string",
+                title: "Order Id",
+                default: "",
+                examples: ["507c7f79bcf86cd7994f6c0e"],
+                pattern: "^(.*)$"
+              },
+              senderId: {
+                type: "string",
+                title: "Sender Id",
+                default: "",
+                examples: ["jkl23;4lnaio34ij3j4io"],
+                pattern: "^(.*)$"
+              },
+              facilityName: {
+                type: "string",
+                title: "The Facility",
+                default: "",
+                examples: ["Indiana Women's Prison"],
+                pattern: "^(.*)$"
+              },
+              status: {
+                type: "string",
+                title: "Order Status",
+                default: "",
+                examples: ["pending"],
+                pattern: "^(.*)$"
+              },
+              addressee: {
+                type: "object",
+                title: "Order Addressee",
+                required: ["firstName", "lastName"],
+                properties: {
+                  firstName: {
+                    type: "string",
+                    title: "addressee first name",
+                    default: "",
+                    examples: ["Mary"],
+                    pattern: "^(.*)$"
+                  },
+                  lastName: {
+                    type: "string",
+                    title: "addressee last name",
+                    default: "",
+                    examples: ["Doe"],
+                    pattern: "^(.*)$"
+                  }
+                }
+              },
+              address: {
+                type: "string",
+                title: "Order Address",
+                default: "",
+                examples: ["2596 N Girls School Rd, Indianapolis, IN 46214"],
+                pattern: "^(.*)$"
+              },
+              unit: {
+                type: "string",
+                title: "addressee unit",
+                default: "",
+                examples: ["F"],
+                pattern: "^(.*)$"
+              },
+              message: {
+                type: "string",
+                title: "Order Message",
+                default: "",
+                examples: [
+                  "Hey Hope You're doing well, your aunt Lin says hello"
+                ],
+                pattern: "^(.*)$"
+              },
+              photos: {
+                type: "array",
+                title: "Orders Photos",
+                items: {
+                  type: "object",
+                  title: "Photo Object"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   },
   {
     method: "GET",
